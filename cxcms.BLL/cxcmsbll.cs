69076fc7219808db43e92cd2de181189aa0c9cb4
italia_r <italia_r@sina.com>
2015-06-05 16:58:19 +0800

    framework.01

---
+++ cxcms.BLL/cxcmsbll.cs
@@ -0,0 +1,13 @@
+﻿//数据库操作相关
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cxcms.BLL
+{
+    public class cxcmsbll
+    {
+    }
+}
