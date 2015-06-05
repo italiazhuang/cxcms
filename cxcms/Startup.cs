b4591346ff0571a85c314c91209234a5f9462c8b
italia_r <italia_r@sina.com>
2015-06-05 15:01:00 +0800

    0.1

---
+++ cxcms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cxcms.Startup))]
+namespace cxcms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
